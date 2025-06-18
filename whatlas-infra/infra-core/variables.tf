1f8acf850a0d4c02b75b6f800746e29297e7ac95
Yuji Arima <yujiarima@hotmail.com>
2025-06-18 11:33:15 -0300

    refactor: changes TerraForm folder structure for core + app resources [infra-app]

---
+++ whatlas-infra/infra-core/variables.tf
@@ -0,0 +1,38 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "southamerica-east1"
+}
+
+variable "scheduler_region" {
+  description = "GCP Scheduler Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "firestore_location" {
+  type        = string
+  default     = "southamerica-east1" 
+  description = "Localização regional do banco Firestore"
+}
+
+variable "cloud_run_service_account_email" {
+  description = "Service account usada pelo Cloud Run para acessar o Secret Manager"
+  type        = string
+}
+
+variable "gcp_sa_key_json" {
+  description = "Conteúdo do JSON da service account"
+  type        = string
+}
+
+variable "image_tag" {
+  description = "Tag da imagem Docker"
+  type        = string
+  default     = "latest"
+}
--- whatlas-infra/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "cloud_run_url" {
-  description = "URL pública da API Whatlas no Cloud Run"
-  value       = google_cloud_run_service.whatlas_api.status[0].url
-}
