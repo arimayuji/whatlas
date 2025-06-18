cc1057933678ee156cc7df6e2e64b99d6e0b9d2f
Yuji Arima <yujiarima@hotmail.com>
2025-06-18 11:40:36 -0300

    chore: add secret.tf resource in [infra-app]

---
+++ whatlas-infra/infra-app/secret.tf
@@ -0,0 +1,17 @@
+resource "google_project_service" "secret"{
+  project = var.project_id
+  service = "secretmanager.googleapis.com"
+}
+
+resource "google_secret_manager_secret" "google_app_credentials" {
+  secret_id = "google-app-credentials_json"
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "google_app_credentials_version" {
+  secret      = google_secret_manager_secret.google_app_credentials.id
+  secret_data = var.gcp_sa_key_json
+}
