1050914139d1e2ad5416b272818049ead3f969ad
Yuji Arima <yujiarima@hotmail.com>
2025-06-19 18:26:44 -0300

    chore: adds handle-error function

---
+++ src/utils/handle-error.ts
@@ -0,0 +1,10 @@
+import { FastifyReply } from "fastify";
+import { AppError } from "../errors/app-error";
+
+export function handleError(error: unknown, reply: FastifyReply) {
+  if (error instanceof AppError) {
+    return reply.code(error.statusCode).send({ error: error.message });
+  }
+
+  return reply.code(500).send({ error: "Internal server error" });
+}
