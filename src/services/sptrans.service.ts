2590885b41f883ec991aa633ae636b430a33c2a3
Yuji Arima <yujiarima@hotmail.com>
2025-06-21 17:20:38 -0300

    refactor: adds repositoy class on sptrans service

--- src/services/sptrans.service.ts
+++ src/services/sptrans.service.ts
@@ -1,138 +1,78 @@
-import { z } from "zod/v4";
-import { http } from "../utils/http";
-
-import { SpTransLine, SpTransLinesModel } from "../models/sptrans-line.model";
-import {
-  SpTransStop,
-  SpTransStopModel,
-  SpTransStopResponseModel,
-} from "../models/sptrans-stop.model";
-import {
-  SpTransCorredor,
-  SpTransCorredorResponseModel,
-} from "../models/sptrans-corredor.model";
-import {
-  SptransCompanyResponse,
-  SptransCompanyResponseModel,
-} from "../models/sptrans-company.model";
-import {
-  LinePosition,
-  LinePositionResponseModel,
-} from "../models/sptrans-position.model";
-import {
-  LineVehiclesPositions,
-  LineVehiclesPositionsModel,
-} from "../models/sptrans-vehicle.model";
-import {
-  VehicleLineETAModel,
-  VehicleLineETA,
-} from "../models/sptrans-eta-line.model";
+import { sptransRepository } from "../repositories/sptrans.repository";
+import { SptransCompanyResponseModel } from "../models/sptrans-company.model";
+import { SpTransCorredorResponseModel } from "../models/sptrans-corredor.model";
+import { VehicleLineETAModel } from "../models/sptrans-eta-line.model";
 import { LinesStopETAModel } from "../models/sptrans-eta-stop.model";
-import {
-  LineStopETA,
-  LineStopETAModel,
-} from "../models/sptrans-eta-stopline.model";
+import { LineStopETAModel } from "../models/sptrans-eta-stopline.model";
+import { SpTransLinesModel } from "../models/sptrans-line.model";
+import { LinePositionResponseModel } from "../models/sptrans-position.model";
+import { SpTransStopResponseModel } from "../models/sptrans-stop.model";
+import { LineVehiclesPositionsModel } from "../models/sptrans-vehicle.model";
 
 export const sptransService = {
   async getCompanies() {
-    const res = await http.get<SptransCompanyResponse>("/Empresas");
-
-    if (res.data.e.length === 0) {
-      return [];
-    }
-
+    const res = await sptransRepository.getCompanies();
     return SptransCompanyResponseModel.parse(res);
   },
 
   async getCorredores() {
-    const res = await http.get<SpTransCorredor[]>("/Corredores");
-
-    if (res.data.length === 0) {
-      return [];
-    }
-
+    const res = await sptransRepository.getCorredores();
     return SpTransCorredorResponseModel.parse(res);
   },
 
   async searchLines(term: string) {
-    const res = await http.get<SpTransLine[]>(
-      `/Linha/Buscar?termosBusca=${term}`
-    );
-
-    if (res.data.length === 0) {
-      return [];
-    }
-
+    const res = await sptransRepository.searchLines(term);
     return SpTransLinesModel.parse(res);
   },
 
   async searchLineWithDirection(term: string, direction: 1 | 2) {
-    const res = await http.get<SpTransLine[]>(
-      `/Linha/BuscarLinhaSentido?termosBusca=${term}&sentido=${direction}`
+    const res = await sptransRepository.searchLineWithDirection(
+      term,
+      direction
     );
-
     return SpTransLinesModel.parse(res);
   },
 
   async getStopsByTerm(term: string) {
-    const res = await http.get<SpTransStop[]>(
-      `/Parada/Buscar?termosBusca=${term}`
-    );
-
+    const res = await sptransRepository.getStopsByTerm(term);
     return SpTransStopResponseModel.parse(res);
   },
 
   async getStopsByLine(lineCode: number) {
-    const res = await http.get<SpTransStop[]>(
-      `/Parada/BuscarParadasPorLinha?codigoLinha=${lineCode}`
-    );
-
+    const res = await sptransRepository.getStopsByLine(lineCode);
     return SpTransStopResponseModel.parse(res);
   },
 
   async getStopsByCorredor(corredorCode: number) {
-    const res = await http.get<SpTransStop[]>(
-      `/Parada/BuscarParadasPorCorredor?codigoCorredor=${corredorCode}`
-    );
-
-    return z.array(SpTransStopModel).parse(res);
+    const res = await sptransRepository.getStopsByCorredor(corredorCode);
+    return SpTransStopResponseModel.parse(res);
   },
 
   async getVehiclePositions() {
-    const res = await http.get<LinePosition>("/Posicao");
-
+    const res = await sptransRepository.getVehiclePositions();
     return LinePositionResponseModel.parse(res);
   },
 
   async getVehiclePositionsByLine(lineCode: number) {
-    const res = await http.get<LineVehiclesPositions>(
-      `/Posicao/Linha?codigoLinha=${lineCode}`
-    );
-
+    const res = await sptransRepository.getVehiclePositionsByLine(lineCode);
     return LineVehiclesPositionsModel.parse(res);
   },
 
   async getArrivalPrediction(stopCode: number, lineCode: number) {
-    const res = await http.get<LineStopETA>(
-      `/Previsao?codigoParada=${stopCode}&codigoLinha=${lineCode}`
+    const res = await sptransRepository.getArrivalPrediction(
+      stopCode,
+      lineCode
     );
-
     return LineStopETAModel.parse(res);
   },
 
   async getArrivalPredictionByLine(lineCode: number) {
-    const res = await http.get<VehicleLineETA>(
-      `/Previsao/Linha?codigoLinha=${lineCode}`
-    );
-
+    const res = await sptransRepository.getArrivalPredictionByLine(lineCode);
     return VehicleLineETAModel.parse(res);
   },
 
   async getArrivalPredictionByStop(stopCode: number) {
-    const res = await http.get<LineStopETA>(
-      `/Previsao/Parada?codigoParada=${stopCode}`
-    );
-
+    const res = await sptransRepository.getArrivalPredictionByStop(stopCode);
     return LinesStopETAModel.parse(res);
   },
 };
