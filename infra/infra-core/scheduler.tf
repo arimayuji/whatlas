bddbc8de5c4dab570b689daa9404f249c20ff9b0
Yuji Arima <yujiarima@hotmail.com>
2025-06-21 12:01:07 -0300

    removes iam resource on [infra-core]

--- infra/infra-core/scheduler.tf
+++ infra/infra-core/scheduler.tf
@@ -3,12 +3,6 @@ resource "google_service_account" "scheduler_sa" {
   display_name = "Scheduler Pub/Sub Publisher"
 }
 
-resource "google_project_iam_member" "scheduler_pubsub_publisher" {
-  role   = "roles/pubsub.publisher"
-  project = var.project_id
-  member = "serviceAccount:service-${google_service_account.scheduler_sa.email}"
-}
-
 resource "google_cloud_scheduler_job" "train_status_job" {
   name        = "train-status-job"
   description = "Publica no tópico de status dos trens a cada 15 min v2"
