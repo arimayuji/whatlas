f106c338c086b7b390275774e4d45c91ab111d01
Yuji Arima <yujiarima@hotmail.com>
2025-06-22 18:38:41 -0300

    chore: updates envs

--- src/env.schema.ts
+++ src/env.schema.ts
@@ -1,9 +1,17 @@
 export const schema = {
   type: "object",
-  required: ["GOOGLE_APPLICATION_CREDENTIALS_JSON", "NODE_ENV", "PORT"],
+  required: [
+    "GOOGLE_APPLICATION_CREDENTIALS_JSON",
+    "NODE_ENV",
+    "PORT",
+    "SPTRANS_TOKEN",
+    "GOOGLE_API_KEY",
+  ],
   properties: {
     GOOGLE_APPLICATION_CREDENTIALS_JSON: { type: "string" },
     NODE_ENV: { type: "string" },
     PORT: { type: "string" },
+    SPTRANS_TOKEN: { type: "string" },
+    GOOGLE_API_KEY: { type: "string" },
   },
 } as const;
