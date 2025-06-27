6850cd31f8508144cd759e9be3bdc460f804f772
Yuji Arima <yujiarima@hotmail.com>
2025-06-27 18:35:22 -0300

    refactor: changes LatLangType name properties

--- src/@types/latlang.type.ts
+++ src/@types/latlang.type.ts
@@ -1,7 +1,7 @@
 import { z } from "zod/v4";
 export const LatLangType = z.object({
-  lat: z.number(),
-  lng: z.number(),
+  latitude: z.number(),
+  longitude: z.number(),
 });
 
 export type LatLang = z.infer<typeof LatLangType>;
