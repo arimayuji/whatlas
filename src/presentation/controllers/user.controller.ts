17f2139674fa0e58d1df272448d176b3691095ff
Yuji Arima <yujiarima@hotmail.com>
2025-07-01 10:35:39 -0300

    chore: adds usecases on controller

--- src/presentation/controllers/user.controller.ts
+++ src/presentation/controllers/user.controller.ts
@@ -1,10 +1,18 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { handleError } from "../../utils/handle-error";
 import { User } from "../../domain/entities/user.model";
-import { UserRepository } from "../../domain/repositories/UserRepository";
+import { CreateUserUseCase } from "../../application/usecases/CreateUserUseCase";
+import { GetUserByIdUseCase } from "../../application/usecases/GetUserByIdUseCase";
+import { GetAllUsersUseCase } from "../../application/usecases/GetAllUsersUseCase";
+import { DeleteUserUseCase } from "../../application/usecases/DeleteUserUseCase";
+import { UpdateUserUseCase } from "../../application/usecases/UpdateUserUseCase";
 
 export class UserController {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(private readonly createUserUseCase: CreateUserUseCase,
+    private readonly updateUserUseCase: UpdateUserUseCase,
+    private readonly getUserByIdUseCase: GetUserByIdUseCase,
+    private readonly getAllUsersUseCase: GetAllUsersUseCase,
+    private readonly deleteUserUseCase: DeleteUserUseCase,) { }
 
   async findById(
     request: FastifyRequest<{ Params: { id: string } }>,
@@ -12,7 +20,8 @@ export class UserController {
   ) {
     try {
       const { id } = request.params;
-      const user = await this.userRepository.findById(id);
+
+      const user = await this.getUserByIdUseCase.execute({ id });
 
       if (!user) return reply.code(404).send({ message: "User not found" });
 
@@ -24,24 +33,9 @@ export class UserController {
 
   async findAll(request: FastifyRequest, reply: FastifyReply) {
     try {
-      const users = await this.userRepository.findAll();
+      const users = await this.getAllUsersUseCase.execute();
+      
       reply.send(users);
-    } catch (error) {
-      handleError(error, reply);
-    }
-  }
-
-  async findByPhone(
-    request: FastifyRequest<{ Querystring: { phone: string } }>,
-    reply: FastifyReply
-  ) {
-    try {
-      const { phone } = request.query;
-      const user = await this.userRepository.findByPhone(phone);
-
-      if (!user) return reply.code(404).send({ message: "User not found" });
-
-      reply.send(user);
     } catch (error) {
       handleError(error, reply);
     }
@@ -55,10 +49,10 @@ export class UserController {
         ...data,
         id: crypto.randomUUID(),
         createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString(),
+        updatedAt: null,
       };
 
-      await this.userRepository.create(user);
+      await this.createUserUseCase.execute(user);
 
       reply.code(201).send(user);
     } catch (error) {
@@ -72,19 +66,12 @@ export class UserController {
   ) {
     try {
       const { id } = request.params;
-      const existing = await this.userRepository.findById(id);
 
-      if (!existing) return reply.code(404).send({ message: "User not found" });
+      const { destinations, defaultOrigin, marginInMinutes } = request.body;
+      
+      await this.updateUserUseCase.execute({id, destinations, defaultOrigin, marginInMinutes,});
 
-      const updated: User = {
-        ...existing,
-        ...request.body,
-        updatedAt: new Date().toISOString(),
-      };
-
-      await this.userRepository.update(id, updated);
-
-      reply.send(updated);
+      reply.send().code(200);
     } catch (error) {
       handleError(error, reply);
     }
@@ -96,11 +83,8 @@ export class UserController {
   ) {
     try {
       const { id } = request.params;
-      const existing = await this.userRepository.findById(id);
 
-      if (!existing) return reply.code(404).send({ message: "User not found" });
-
-      await this.userRepository.delete(id);
+      await this.deleteUserUseCase.execute({ id });
 
       reply.code(204).send();
     } catch (error) {
