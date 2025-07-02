8b55ba03b1504477a0e9df3ba108672db6b2fe51
Yuji Arima <yujiarima@hotmail.com>
2025-07-02 17:57:34 -0300

    refactor: adds repositories singletons

---
+++ src/infra/repositories/index.ts
@@ -0,0 +1,16 @@
+import { createSupabaseClient } from "../clients/supabase.client";
+import { BalanceRepositoryFirestore } from "./balance.repository";
+import { GeoRepository } from "./geo.repository";
+import { RechargeRepositoryFirestore } from "./recharge.repository";
+import { TrainStatusFirestoreRepository } from "./train-status.repository";
+import { TripRepositoryFirestorre } from "./trip.repository";
+import { UserRepositoryFirestore } from "./user.repository";
+
+export const repositories = {
+  userRepository: new UserRepositoryFirestore(),
+  geoRepository: new GeoRepository(createSupabaseClient()),
+  tripRepository: new TripRepositoryFirestorre(),
+  trainStatusRepository: new TrainStatusFirestoreRepository(),
+  rechargeRepository: new RechargeRepositoryFirestore(),
+  balanceRepository: new BalanceRepositoryFirestore(),
+}
