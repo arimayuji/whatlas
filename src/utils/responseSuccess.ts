756923e122489d8f9218e75b6ce2b028bb589c09
Yuji Arima <yujiarima@hotmail.com>
2025-07-02 22:57:17 -0300

    chore: remove import

--- src/utils/responseSuccess.ts
+++ src/utils/responseSuccess.ts
@@ -1,5 +1,4 @@
 import { FastifyReply } from "fastify";
-import { success } from "zod/v4";
 
 export function responseSuccess(reply: FastifyReply, data: any, message = "Success", code = 200) {
   reply.code(200).send({
