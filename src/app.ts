b8f8e842e2f25a6e341035e698405ee947117e38
Yuji Arima <yujiarima@hotmail.com>
2025-07-03 21:05:44 -0300

    chore: adds hooks for response logs

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,6 @@ import { schema } from "./env.schema";
 import { userRoutes } from "./presentation/routes/user.route";
 import { GoogleApiController } from "./presentation/controllers/google.controller";
 import { HttpGoogleApiGateway } from "./infra/gateways/google-api.gateway";
-import { TrainStatusController } from "./presentation/controllers/train-status.controller";
-import { TrainStatusFirestoreRepository } from "./infra/repositories/train-status.repository";
 import { googleApiRoutes } from "./presentation/routes/google.route";
 import { trensStatusRoute } from "./presentation/routes/trens-status.route";
 import { sptransRoutes } from "./presentation/routes/sptrans.route";
@@ -27,19 +25,26 @@ import { createSupabaseClient } from "./infra/clients/supabase.client";
 import { handleError } from "./utils/handle-error";
 import { makeUserController } from "./infra/factories/user-controller.factory";
 import { makeSpTransController } from "./infra/factories/sptrans-controller.factory";
+import { makeTrainStatusController } from "./infra/factories/train-status.factory";
+import {  tripRoutes } from "./presentation/routes/trip.route";
+import { makeTripController } from "./infra/factories/trip.controller";
 
 dotenv.config();
 const googleController = new GoogleApiController(new HttpGoogleApiGateway());
 const userController = makeUserController();
 const spTransController = makeSpTransController();
+const tripController = makeTripController();
 
-const trensController = new TrainStatusController(new TrainStatusFirestoreRepository());
+const trainStatusController = makeTrainStatusController();
 const supabase = createSupabaseClient();
 const findNearestController = new FindNearestStopController(
   new FindNearestStopUseCase(new GeoRepository(supabase))
 );
 
-const app = fastify({ logger: true }).withTypeProvider<ZodTypeProvider>();
+const app = fastify({
+  logger: {
+    level: 'info',
+  },}).withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -74,27 +79,50 @@ const start = async () => {
       googleApiRoutes(instance, googleController);
       done();
     });
+
     await app.register((instance, opts, done) => {
       findNearestStopRoutes(instance, findNearestController);
       done();
     });
+
     await app.register((instance, opts, done) => {
       userRoutes(instance, userController);
       done();
     });
+
     await app.register((instance, opts, done) => {
-      trensStatusRoute(instance, trensController);
+      trensStatusRoute(instance, trainStatusController);
       done();
     });
+
     await app.register((instance, opts, done) => {
       sptransRoutes(instance, spTransController);
       done();
     });
 
+    await app.register((instance, opts, done) => {
+      tripRoutes(instance, tripController);
+      done();
+    })
+
     const port = parseInt(process.env.PORT || "8080", 10);
+
     if (isNaN(port)) {
       throw new Error("Invalid port number");
     }
+
+    app.addHook('onResponse', (req, res, done) => {
+      const status = res.statusCode;
+      if (status >= 200 && status < 300) {
+        req.log.info({
+          method: req.method,
+          url: req.url,
+          status,
+          duration: res.elapsedTime,
+        }, 'request_success');
+      }
+      done();
+    });
 
     await app.listen({ port, host: "0.0.0.0" });
     app.log.info(`🚀 Server running on port ${port}`);
