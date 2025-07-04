990b48ac222be478c8259dde621673408a646545
Yuji Arima <yujiarima@hotmail.com>
2025-07-03 21:08:17 -0300

    chore: adds usecases on train-status controller

--- src/presentation/controllers/train-status.controller.ts
+++ src/presentation/controllers/train-status.controller.ts
@@ -1,26 +1,43 @@
 import { FastifyRequest, FastifyReply } from "fastify";
-import { TrainStatusRepository } from "../../domain/repositories/TrainStatusRepository";
 import { responseSuccess } from "../../utils/responseSuccess";
+import { GetTrainStatusById } from "../../application/usecases/GetTrainStatusByIdUseCase";
+import { GetTrainStatusByLine } from "../../application/usecases/GetTrainStatusByLineUseCase";
+import { FindAllTrainStatusByIdUseCase } from "../../application/usecases/FindAllTrainStatusUseCase";
 
 export class TrainStatusController {
-  constructor(private readonly trainStatusRepository: TrainStatusRepository) {}
+  constructor(private readonly getTrainStatusByIdUseCase: GetTrainStatusById, private readonly getTrainStatusByLineUseCase: GetTrainStatusByLine,
+      private readonly getAllTrainStatusUseCase: FindAllTrainStatusByIdUseCase,
+  ) {}
 
   async findAll(_: FastifyRequest, reply: FastifyReply) {
-      const data = await this.trainStatusRepository.getAll();
-      return responseSuccess(reply, data, "Founded all lines", 200);
+    const data = await this.getAllTrainStatusUseCase.execute();
+    
+    return responseSuccess(reply, data, "Founded all lines", 200);
   }
 
   async findById(
     request: FastifyRequest<{ Params: { id: string } }>,
     reply: FastifyReply
   ) {
-      const { id } = request.params;
-      const data = await this.trainStatusRepository.getById(id);
+    const { id } = request.params;
+    
+    const data = await this.getTrainStatusByIdUseCase.execute({ id });
 
-      if (!data) {
+    if (!data) {
         return reply.code(404).send({ message: "Linha não encontrada" });
       }
 
-      return responseSuccess(reply, data, "Founded line", 200);
+    return responseSuccess(reply, data, "Founded line", 200);
+  }
+
+  async getByLine(
+    request: FastifyRequest<{ Params: { line: string } }>,
+    reply: FastifyReply
+  ) {
+    const { line } = request.params;
+
+    const data = await this.getTrainStatusByLineUseCase.execute({ line });      
+    
+    return responseSuccess(reply, data, "Founded line", 200);
   }
 }
