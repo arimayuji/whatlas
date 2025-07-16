076a02ca63504581c75d37f5e737fa74b59fc08a
Yuji Arima <yujiarima@hotmail.com>
2025-07-16 11:33:38 -0300

    chore: deletes unecessary use-cases

--- src/application/usecases/FindAllTrainStatusUseCase.ts
+++
@@ -1,9 +0,0 @@
-import { TrainStatusRepository } from "../../domain/repositories/TrainStatusRepository"
-
-export class FindAllTrainStatusByIdUseCase {
-  constructor(private readonly trainStatusRepository: TrainStatusRepository) {}
-  
-  async execute() {
-    return this.trainStatusRepository.getAll()
-  }
-}
