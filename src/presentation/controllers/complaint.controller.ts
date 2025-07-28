6fc28413fbcf0c7d4e18043442f58886802edbec
Yuji Arima <yujiarima@hotmail.com>
2025-07-27 22:08:54 -0300

    feat: implement complaint-vote controller

--- src/presentation/controllers/complaint.controller.ts
+++ src/presentation/controllers/complaint.controller.ts
@@ -2,7 +2,7 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { CreateComplaintUseCase } from "../../application/usecases/CreateComplaintUseCase";
 import { FindAllComplaintsUseCase } from "../../application/usecases/FindAllComplaintsUseCase";
 import { GetComplaintsBySubjectTypeUseCase } from "../../application/usecases/GetComplaintsBySubjectTypeUseCase";
-import { Complaint } from "../../domain/entities/complaint.model";
+import { responseSuccess } from "../../utils/responseSuccess";
 
 export class ComplaintController {
   constructor(
@@ -11,16 +11,26 @@ export class ComplaintController {
     private readonly getComplaintsBySubjectTypeUseCase: GetComplaintsBySubjectTypeUseCase,
   ) { }
   
-  async createComplaint(req: FastifyRequest<{ Body: Complaint }>, res: FastifyReply) {
-    const {createdAt,downVotes,id,subjectId,subjectType,text,upVotes} = req.body
+  async createComplaint(req: FastifyRequest<{ Body: { subjectType: 'terminal_onibus' | 'linhas_metro' | 'linhas_trem' | 'linhas_onibus', text: string} }>, res: FastifyReply) {
+    const { subjectType, text } = req.body
+    
+    const result = await this.createComplaintUseCase.exec({subjectType, text});
+
+    return responseSuccess(res, { data: result, message: "Complaint created successfully", code: 201 });
   }
   
   async findAllComplaints(req: FastifyRequest, res: FastifyReply) {
-
+    const result = await this.findAllComplaintsUseCase.exec();
+    
+    return responseSuccess(res, { data: result, message: "Complaints retrieved successfully", code: 200 });
    }
   
   async getComplaintsBySubjectType(req: FastifyRequest<{ Params: { subjectType: string } }>, res: FastifyReply) {
     const { subjectType } = req.params
+
+    const result = await this.getComplaintsBySubjectTypeUseCase.exec({ subjectType });  
+
+    return responseSuccess(res, { data: result, message: "Complaints by subject type retrieved successfully", code: 200 });
   }
 
 }
